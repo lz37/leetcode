7f6b492bbc410a1e347178bc69b979edcb0cab04
zerozawa <zekaixie37@gmail.com>
2022-07-10 11:24:29 +0800

    [543] 二叉树的直径

--- 543.二叉树的直径.cpp
+++ 543.二叉树的直径.cpp
@@ -26,7 +26,37 @@ struct TreeNode {
  * };
  */
 class Solution {
+#ifndef max
+#define max(x, y) x > y ? x : y
+#endif
+private:
+  int maxDiameter = 0;
+  void dfsCalc(TreeNode *root) {
+    if (!root)
+      return;
+    dfsCalc(root->left);
+    dfsCalc(root->right);
+    int leftLength = root->left ? root->left->val + 1 : 0;
+    int rightLength = root->right ? root->right->val + 1 : 0;
+    root->val = max(leftLength, rightLength);
+    maxDiameter = max(maxDiameter, leftLength + rightLength);
+  }
+
 public:
-  int diameterOfBinaryTree(TreeNode *root) {}
+  /**
+   * @brief
+   * 104/104 cases passed (4 ms)
+   * Your runtime beats 94.78 % of cpp submissions
+   * Your memory usage beats 92.52 % of cpp submissions (19.6 MB)
+   *
+   * @param root
+   * @return int
+   */
+  int diameterOfBinaryTree(TreeNode *root) {
+    if (!root)
+      return 0;
+    dfsCalc(root);
+    return maxDiameter;
+  }
 };
 // @lc code=end
